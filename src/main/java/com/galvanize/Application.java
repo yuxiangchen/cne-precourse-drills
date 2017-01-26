c322d3f3a9a06551bc283836a55b54c267b83769
Jeff Dean <jeff@galvanize.com>
2017-01-26 10:05:35 -0700

    initial commit

---
+++ src/main/java/com/galvanize/Application.java
@@ -0,0 +1,20 @@
+package com.galvanize;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Gson builder = new GsonBuilder()
+                .setPrettyPrinting()
+                .create();
+        JsonObject object = new JsonObject();
+        System.getProperties().stringPropertyNames().iterator().forEachRemaining(propertyName ->
+                object.addProperty(propertyName, System.getProperty(propertyName))
+        );
+        System.out.println(builder.toJson(object));
+    }
+
+}
---
+++ src/test/java/com/galvanize/LibraryTest.java
@@ -0,0 +1,12 @@
+package com.galvanize;
+
+import com.galvanize.Library;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LibraryTest {
+    @Test public void testSomeLibraryMethod() {
+        Library classUnderTest = new Library();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
