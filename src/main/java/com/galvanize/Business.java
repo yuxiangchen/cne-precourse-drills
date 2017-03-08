a48cfe79121caee9c4234ac37f51a2db3824749e
yuxiang.chen <yuxiang.chen@dish.com>
2017-03-07 21:22:15 -0700

    Update Business addresses type into final

--- src/main/java/com/galvanize/Business.java
+++ src/main/java/com/galvanize/Business.java
@@ -1,14 +1,14 @@
 package com.galvanize;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Business implements Addressable {
     private final String name;
-    private ArrayList<Address> addresses;
+    private final ArrayList<Address> addresses;
 
     public Business(String name) {
         this.name = name;
+        this.addresses = new ArrayList<>();
     }
 
     public String getName() {
@@ -17,17 +17,14 @@ public class Business implements Addressable {
 
 
     @Override
-    public List<Address> getAddresses() {
+    public ArrayList<Address> getAddresses() {
 
         return addresses;
-//        return null;
     }
 
     @Override
     public void addAddress(Address address) {
         addresses.add(address);
-//        List<Address> temp = new ArrayList<>();
-//        temp.add(address);
+
     }
 }
-
